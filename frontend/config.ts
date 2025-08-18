c5051174195e4d2ab983557ac57dd3753a7331e5
Leap <git@leap.new>
2025-08-18 17:31:30 +0000

    Manual code change

--- frontend/config.ts
+++ frontend/config.ts
@@ -4,4 +4,4 @@
 // 2. Select your application.
 // 3. Navigate to "API Keys".
 // 4. Copy the "Publishable key" and paste it here.
-export const clerkPublishableKey = "";
+export const clerkPublishableKey = "pk_test_cHJlcGFyZWQtaGVuLTUuY2xlcmsuYWNjb3VudHMuZGV2JA";
