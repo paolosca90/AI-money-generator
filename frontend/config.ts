bfcaa5e2221bc6e0a08806cafd0dc9815ec5f6cf
Leap <git@leap.new>
2025-08-18 17:24:30 +0000

    Fix Clerk publishable key configuration

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,3 +1,3 @@
 // The Clerk publishable key, to initialize Clerk.
 // TODO: Set this to your Clerk publishable key, which can be found in the Clerk dashboard.
-export const clerkPublishableKey = "pk_test_ZGVhci1wb255LTQxLmNsZXJrLmFjY291bnRzLmRldiQ";
+export const clerkPublishableKey = "";
